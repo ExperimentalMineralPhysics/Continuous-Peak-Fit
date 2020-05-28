0d4c1ed7656ed6488b449aa24dc2b1cd8b35a6fa
S-Hunt <7522838+S-Hunt@users.noreply.github.com>
2020-05-28 10:21:05 +0100

    Update example
    Update input file, remove some surplus input files.

--- Example1-Fe/BCC1_MultiPeak_input_Dioptas.py
+++ Example1-Fe/BCC1_MultiPeak_input_Dioptas.py
@@ -1,14 +1,7 @@
-# Input parameters for BCC1_2GPa_100s_001. 
-#
-# It is an attempt to collate all the information needed in the fitting routines.
-# A 'proper' input file type can be formuated later.
+# Input parameters for Example 1 -- BCC1_2GPa_100s_001. 
 
 # properties of the data files.
-<<<<<<< HEAD
 datafile_directory = './'
-=======
-datafile_directory = './Example1Fe/'
->>>>>>> python3
 datafile_Basename  = 'BCC1_2GPa_10s_001_'
 datafile_Ending    = '.tif'
 datafile_StartNum  = 1
@@ -16,22 +9,17 @@ datafile_EndNum    = 10
 datafile_NumDigit  = 5
 
 # Calibration and masking.
-Calib_type   = "Dioptas"
+Calib_type     = "Dioptas"
 Calib_detector = 'Pilatus1M'
-<<<<<<< HEAD
-Calib_data     = 'CeO2_Pil207_E30_2Nov2016_001.tif'
-Calib_param    = 'CeO2_cal_Dioptas.poni'
-Calib_mask     = 'DiffractionMask_Dioptas.mask'
-=======
 Calib_data     = datafile_directory + 'CeO2_Pil207_E30_2Nov2016_001.tif'
 Calib_param    = datafile_directory + 'CeO2_cal_Dioptas.poni'
 Calib_mask     = datafile_directory + 'DiffractionMask_Dioptas.mask'
->>>>>>> python3
-Calib_pixels = 172
+Calib_pixels   = 172
 
 # Fitting properties for peaks.
 fit_orders = [
        {
+         "range": [[10.8, 11.67]],
          "background": [0,0],
          "peak": [{
              "phase": "Other",
@@ -43,7 +31,7 @@ fit_orders = [
              "width": 0,
              "symmetry": 2
            },{
-             "phase": "BCC-Fe",
+             "phase": "Fe-BCC",
              "hkl": 110,
              "d-space": 3,
              "height": 16,
@@ -52,13 +40,34 @@ fit_orders = [
              "width": 0,
              "symmetry": 2
            }],
-         "range": [[10.8, 11.67]],
-         "PeakPositionSelection": [[1,-120.5,11.19],[1,-58.997,11.19],[1,59.289,11.19],[1,23.187,11.19],[1,23.212,11.19], [1,23.158,11.19], [1,23.246,11.19], [2,-120, 11.54], [2,-90, 11.54], [2,-45, 11.54], [2,-0, 11.54], [2,45, 11.54], [2,90, 11.54], [2,120, 11.54], [2,179, 11.54]]
+         "PeakPositionSelection": [[1,-170.5,  11.19],
+                                   [1,-150.5,  11.19],
+                                   [1,-120.5,  11.19],
+                                   [1, -58.99, 11.19],
+                                   [1,  59.289,11.19],
+                                   [1,  23.187,11.19],
+                                   [1,  23.212,11.19], 
+                                   [1,  53.158,11.19], 
+                                   [1,  73.246,11.19], 
+                                   [1,  93.246,11.19], 
+                                   [1, 123.246,11.19],
+                                   [2,-170,    11.54],
+                                   [2,-150,    11.54], 
+                                   [2,-110,    11.54], 
+                                   [2, -90,    11.54], 
+                                   [2, -45,    11.54], 
+                                   [2,  -0,    11.54], 
+                                   [2,  45,    11.54],
+                                   [2,  90,    11.54], 
+                                   [2, 120,    11.54], 
+                                   [2, 150,    11.54], 
+                                   [2, 179,    11.54]]
        },
        {
+         "range": [[16.1,16.6]],
          "background": [0,0],
          "peak": [{
-             "phase": "BCC-Fe",
+             "phase": "Fe-BCC",
              "hkl": 200,
              "d-space": 3,
              "height": 8,
@@ -67,12 +76,12 @@ fit_orders = [
              "width": 0,
              "symmetry": 2
            }],
-         "range": [[16.1,16.6]]
        },
        {
+         "range": [[19.8, 20.27]],
          "background": [0,0],
          "peak": [{
-             "phase": "BCC-Fe",
+             "phase": "Fe-BCC",
              "hkl": 211,
              "d-space": 3,
              "height": 8,
@@ -81,27 +90,33 @@ fit_orders = [
              "width": 0,
              "symmetry": 2
            }],
-         "range": [[19.8, 20.27]]
        },
-      {
-          "background": [1],
-          "peak": [{
-             "phase": "BCC-Fe",
-             "hkl": 220,
-              "d-space": 3,
-              "height": 8,
-              "profile": 0,
-              "profile_fixed": 0.5,
-              "width": 0,
-              "symmetry": 2
-            }],
-#          "PeakPositionSelection": [[1,-120.5,23.226],[1,-58.997,23.186],[1,59.289,23.208],[1,23.187,120.893],[1,23.212,6.367], [1,23.158,90.025], [1,23.246,25.729]]
-          "range": [[23.1, 23.5]]
-        },
        {
+         "range": [[23.1, 23.5]],
+         "background": [1],
+         "peak": [{
+             "phase": "Fe-BCC",
+             "hkl": 220,
+             "d-space": 3,
+             "height": 8,
+             "profile": 0,
+             "profile_fixed": 0.5,
+             "width": 0,
+             "symmetry": 2
+           }],
+          # "PeakPositionSelection": [[1,-120.5,  23.226],
+          #                           [1,-58.997, 23.186],
+          #                           [1, 59.289, 23.208],
+          #                           [1, 23.187,120.893],
+          #                           [1, 23.212,  6.367], 
+          #                           [1, 23.158, 90.025], 
+          #                           [1, 23.246, 25.729]]
+       },
+       {
+         "range": [[25.7, 26.4]],
          "background": [1,1],
          "peak": [{
-             "phase": "BCC-Fe",
+             "phase": "Fe-BCC",
              "hkl": 310,
              "d-space": 2,
              "height": 1,
@@ -110,15 +125,15 @@ fit_orders = [
              "width": 0,
              "symmetry": 2
            }],
-         "range": [[25.7, 26.4]]
        },
     ]
 
 #Number of bins for initial fitting.
 AziBins = 90
 
-
 #Output settings
 Output_directory   = './'
-Output_type        = 'MultiFit'
+Output_type        = 'Polydefix'
 Output_NumAziWrite = 90
+phase              = 'Fe-BCC'
+Output_ElasticProperties = 'Properties_Fe-BCC.txt'

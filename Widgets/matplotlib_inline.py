84be323c2e8e1900b1e5d4f2d8de8a4b0f941cc7
AdinaRahim <adinarahim96@gmail.com>
2022-11-25 22:26:46 +0000

    Enable matplotlib inline

---
+++ Widgets/matplotlib_inline.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 25 21:59:16 2022
+
+@author: g05296ar
+"""
+
+#%matplotlib auto
+
+import matplotlib.pyplot as pl
+
+
+class matplotlib_inline():
+    def __init__(self):
+        super(matplotlib_inline, self).__init__()
+        try:
+            get_ipython().magic("matplotlib inline")
+        except:
+            pl.ion()
+        import numpy as np

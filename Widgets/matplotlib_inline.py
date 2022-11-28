98e065b1b2453c89c7d53bc60bc16540bb93739a
AdinaRahim <adinarahim96@gmail.com>
2022-11-28 13:06:11 +0000

    Rename matplotlib_inline to matplotlib_auto

--- Widgets/matplotlib_inline.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as pl
-
-class matplotlib_inline():
-    def __init__(self):
-        super(matplotlib_inline, self).__init__()
-        try:
-            get_ipython().magic("matplotlib inline")
-        except:
-            pl.ion()

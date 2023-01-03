987db546dc7c58ab9a8f6aa270f6888e719035ac
AdinaRahim <adinarahim96@gmail.com>
2023-01-03 15:42:01 +0000

    Remove templating code for peaks

--- Widgets/Peak_Widget.py
+++ Widgets/Peak_Widget.py
@@ -21,7 +21,40 @@ class Peak(QWidget):
         super(Peak, self).__init__()
         loadUi("Peak_Widget.ui", self)
         self.peak_layout()
+        self.profile_fixed.setEnabled(False)
+        self.width_fixed.setEnabled(False)
+        self.height_fixed.setEnabled(False)
+        self.dspace_fixed.setEnabled(False)
         
+        self.profile_checkBox.stateChanged.connect(lambda:profilestatechange())
+        self.width_checkBox.stateChanged.connect(lambda:widthstatechange())
+        self.height_checkBox.stateChanged.connect(lambda:heghtstatechange())
+        self.dspace_checkBox.stateChanged.connect(lambda:dspacestatechange())
+        
+        def profilestatechange():
+            if self.profile_checkBox.isChecked()==True:
+               self.profile_fixed.setEnabled(True)
+            else:
+               self.profile_fixed.setEnabled(False)
+          
+        def widthstatechange():
+            if self.width_checkBox.isChecked()==True:
+               self.width_fixed.setEnabled(True)
+            else:
+               self.width_fixed.setEnabled(False)
+          
+        def heghtstatechange():
+            if self.height_checkBox.isChecked()==True:
+               self.height_fixed.setEnabled(True)
+            else:
+               self.height_fixed.setEnabled(False)
+          
+        def dspacestatechange():
+            if self.dspace_checkBox.isChecked()==True:
+               self.dspace_fixed.setEnabled(True)
+            else:
+               self.dspace_fixed.setEnabled(False)
+          
     def peak_layout(self):
         self.phase_peak.setMinimumHeight(40);
         self.hkl.setMinimumHeight(40);
@@ -38,107 +71,4 @@ class Peak(QWidget):
         self.height_peak_type.setMinimumHeight(40);
         self.profile_peak_type.setMinimumHeight(40);
         self.width_peak_type.setMinimumHeight(40);
-        self.Save_Peak_Btn.clicked.connect(self.Save_Peaks)
-        self.clickcount = 1
-        
-    def Save_Peaks(self):
-        phase_peak = self.phase_peak.text()
-        hkl = self.hkl.text()
-        d_space_peak = self.d_space_peak.text()
-        d_space_type = self.d_space_type.currentText()
-        dspace_fixed = self.dspace_fixed.text()
-        height_peak = self.height_peak.text()
-        height_peak_type = self.height_peak_type.currentText()
-        height_fixed =self.height_fixed.text()
-        profile_peak = self.profile_peak.text()
-        profile_peak_type = self.profile_peak_type.currentText()
-        profile_fixed = self.profile_fixed.text()
-        width_peak = self.width_peak.text()
-        width_peak_type =self.width_peak_type.currentText()
-        width_fixed = self.width_fixed.text()
-        symmetry_peak = self.symmetry_peak.text()
-        
-        if (self.clickcount == 1) :
-        
-            data =  " \n\
-               \"peak\": [{                                              \n\
-                          \"phase\": \"$phase_peak\",                               \n\
-                          \"hkl\": '$hkl',                                        \n\
-                          \"d-space\": $d_space_peak,                                     \n\
-                          \"d-space-type\": \"$d_space_type\",                 \n\
-                          \"d-space_fixed\": $dspace_fixed,                      \n\
-                          \"height\": $height_peak,                                       \n\
-                          \"height-type\": \"$height_peak_type\",                           \n\
-                          \"height_fixed\": $height_fixed,   \n\
-                          \"profile\": $profile_peak,                                     \n\
-                          \"profile-type\": $profile_peak_type       \n\
-                          \"profile_fixed\": $profile_fixed,                             \n\
-                          \"width\": $width_peak,                                       \n\
-                          \"width-type\": $width_peak_type,       \n\
-                          \"width_fixed\": $width_fixed ,                \n\
-                          \"symmetry\": $symmetry_peak                                     \n\
-                        },"
-            temp_obj = Template(data)
-            after_replacing = temp_obj.substitute(phase_peak=phase_peak, 
-                                                  hkl=hkl,
-                                                  d_space_peak= d_space_peak, 
-                                                  d_space_type= d_space_type,
-                                                  dspace_fixed=dspace_fixed,
-                                                  height_peak=height_peak,
-                                                  height_peak_type =height_peak_type,
-                                                  height_fixed =height_fixed,
-                                                  profile_peak= profile_peak,
-                                                  profile_peak_type  = profile_peak_type,
-                                                  profile_fixed = profile_fixed,
-                                                  width_peak =width_peak,
-                                                  width_peak_type= width_peak_type,
-                                                  width_fixed  =width_fixed ,
-                                                  symmetry_peak = symmetry_peak
-                                                  )
-            file1 = open('input.py', 'a')
-            file1.writelines((after_replacing))
-            file1.close()
-        else:
-            
-            data =       "\n\ {                                              \n\
-                               \"phase\": \"$phase_peak\",                               \n\
-                               \"hkl\": '$hkl',                                        \n\
-                               \"d-space\": $d_space_peak,                                     \n\
-                               \"d-space-type\": \"$d_space_type\",                 \n\
-                               \"d-space_fixed\": $dspace_fixed,                      \n\
-                               \"height\": $height_peak,                                       \n\
-                               \"height-type\": \"$height_peak_type\",                           \n\
-                               \"height_fixed\": $height_fixed,   \n\
-                               \"profile\": $profile_peak,                                     \n\
-                               \"profile-type\": $profile_peak_type       \n\
-                               \"profile_fixed\": $profile_fixed,                             \n\
-                               \"width\": $width_peak,                                       \n\
-                               \"width-type\": $width_peak_type,       \n\
-                               \"width_fixed\": $width_fixed ,                \n\
-                               \"symmetry\": $symmetry_peak                                     \n\
-                             },"
-           
-    
-            
-            temp_obj = Template(data)
-            after_replacing = temp_obj.substitute(phase_peak=phase_peak, 
-                                                  hkl=hkl,
-                                                  d_space_peak= d_space_peak, 
-                                                  d_space_type = d_space_type,
-                                                  dspace_fixed=dspace_fixed,
-                                                  height_peak=height_peak,
-                                                  height_peak_type =height_peak_type,
-                                                  height_fixed =height_fixed,
-                                                  profile_peak= profile_peak,
-                                                  profile_peak_type  = profile_peak_type,
-                                                  profile_fixed = profile_fixed,
-                                                  width_peak =width_peak,
-                                                  width_peak_type= width_peak_type,
-                                                  width_fixed  =width_fixed ,
-                                                  symmetry_peak = symmetry_peak
-                                                  )
-            file1 = open('Input.py', 'a')
-            file1.writelines((after_replacing))
-            file1.close()
-        self.clickcount +=1
-        self.Save_Peak_Btn.setDisabled(True)
+       

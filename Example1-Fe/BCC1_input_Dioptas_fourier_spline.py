177179f0456e9c54cf13075dfb8930910eee1f6f
AdinaRahim <adinarahim96@gmail.com>
2023-01-15 15:48:51 +0000

    Change paths in rest of the input files

--- Example1-Fe/BCC1_input_Dioptas_fourier_spline.py
+++ Example1-Fe/BCC1_input_Dioptas_fourier_spline.py
@@ -1,7 +1,7 @@
 # Input parameters for Example 1 -- BCC1_2GPa_100s_001. 
 
 # properties of the data files.
-datafile_directory = '../Example1-Fe/'
+datafile_directory = 'Example1-Fe/'
 datafile_Basename  = 'BCC1_2GPa_10s_001_'
 datafile_Ending    = '.tif'
 datafile_StartNum  = 1
@@ -205,8 +205,8 @@ fit_orders = [
 AziBins = 90
 
 #Output settings
-Output_directory   = '../Example1-Fe/results/'
+Output_directory   = 'Example1-Fe/results/'
 Output_type        = ['DifferentialStrain', 'Polydefix']
 Output_NumAziWrite = 90
 phase              = 'Fe-BCC'
-Output_ElasticProperties = '../Example1-Fe/Properties_Fe-BCC.txt'
+Output_ElasticProperties = 'Example1-Fe/Properties_Fe-BCC.txt'

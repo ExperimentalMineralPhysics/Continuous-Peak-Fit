ead7abcdc127a75a8188570333bb4ad8e15e5536
AdinaRahim <adinarahim96@gmail.com>
2023-01-19 22:37:53 +0000

    Revert Changes

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import sys
-import os
 from PyQt6.QtWidgets import QApplication
+import os
 
 # Add our sub directories to the module search path
 sys.path.append('Widgets')
 sys.path.append('cpf')
 
-from Main_Widget import MainWindow
+from Main_Widget import Main_Widget
 
 def main():
     # Instantiate the QT application
-    app = QApplication(sys.argv)
-    mainwindow = MainWindow()
-    mainwindow.show() 
-    try:        
+    try: 
+        app = QApplication(sys.argv)
+        main_widget = Main_Widget()
+        main_widget.showMaximized()
         sys.exit(app.exec())
     except Exception as e:
         print("ERROR:", e)
         os._exit(00)
+  
 	  
 if __name__ == '__main__':
-    main()
+	main()

c0f0678b0b3c83af191f4df3bab66e7df4812792
S-Hunt <7522838+S-Hunt@users.noreply.github.com>
2023-03-12 08:45:24 +0000

    add new criteria to thresholds
    only make the mask if the max/min is outside a value

--- cpf/BrightSpots.py
+++ cpf/BrightSpots.py
@@ -16,14 +16,24 @@ Current useage of intensity limits:
     
     "imin": %i
     "imin": "[0<%f<100]%"
-    
+    "imin": {"percentile": "[0<%f<100]%",
+             "minbelow": 20}
+    "imax": {"limit": "[0<%f<100]%",
+             "abovebelow": 200}
     e.g.     
     "imin": 234
     "imin": "99.9%"
+    "imax": {"limit": 300,
+         "abovebelow": 200}
+    "imax": {"limit": "99%",
+         "abovebelow": 200}
     
+       
     Where a numberical value masks the data above/below that value and the percentage
     as a string masks above or below that percentile.
     
+    The combined condition only applies the limit condition if the maximum or minimum is 
+    above or below the "abovebelow" value. 
 
 Simon Hunt, March 2023
 """
@@ -39,13 +49,27 @@ def SpotProcess(sub_data, settings_for_fit):
     if "imax" in settings_for_fit.subfit_orders:
         if isinstance(settings_for_fit.subfit_orders["imax"], str):
             imax = np.percentile(sub_data.intensity.flatten(), float(settings_for_fit.subfit_orders["imax"].strip('%')))
+        elif isinstance(settings_for_fit.subfit_orders["imax"], dict):
+            if np.max(sub_data.intensity.flatten()) > settings_for_fit.subfit_orders["imax"]["abovebelow"]:
+                if isinstance(settings_for_fit.subfit_orders["imax"]["limit"], str):
+                    imax = np.percentile(sub_data.intensity.flatten(), float(settings_for_fit.subfit_orders["imax"]["limit"].strip('%')))
+                else:
+                    imax = int(settings_for_fit.subfit_orders["imax"]["limit"])
         else:
             imax = int(settings_for_fit.subfit_orders["imax"])
+            
     if "imin" in settings_for_fit.subfit_orders:
         if isinstance(settings_for_fit.subfit_orders["imin"], str):
             imin = np.percentile(sub_data.intensity.flatten(), float(settings_for_fit.subfit_orders["imin"].strip('%')))
+        elif isinstance(settings_for_fit.subfit_orders["imin"], dict):
+            if np.max(sub_data.intensity.flatten()) > settings_for_fit.subfit_orders["imin"]["abovebelow"]:
+                if isinstance(settings_for_fit.subfit_orders["imin"]["limit"], str):
+                    imax = np.percentile(sub_data.intensity.flatten(), float(settings_for_fit.subfit_orders["imin"]["limit"].strip('%')))
+                else:
+                    imax = int(settings_for_fit.subfit_orders["imin"]["limit"])
         else:
             imin = int(settings_for_fit.subfit_orders["imin"])
+            
     sub_data.set_mask(i_min=imin, i_max=imax)
 
     return sub_data

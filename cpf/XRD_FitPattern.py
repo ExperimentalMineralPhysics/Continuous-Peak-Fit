3ac45ced1137f95870df2e0c0cf1449f18f9f629
S-Hunt <7522838+S-Hunt@users.noreply.github.com>
2023-03-13 13:45:20 +0000

    add SpotProcess to header

--- cpf/XRD_FitPattern.py
+++ cpf/XRD_FitPattern.py
@@ -11,6 +11,7 @@ import pathos.multiprocessing as mp
 from cpf.settings import settings
 from cpf.XRD_FitSubpattern import fit_sub_pattern
 from cpf.Cosmics import image_preprocess as cosmicsimage_preprocess
+from cpf.BrightSpots import SpotProcess
 from cpf.IO_functions import (
     json_numpy_serializer,
     make_outfile_name,

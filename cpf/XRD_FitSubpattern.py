0d528a7fe6a4da939d7969f9e4b6a05c5a7d00a4
S-Hunt <7522838+S-Hunt@users.noreply.github.com>
2024-06-18 22:04:39 +0100

    Removed d-space array
    part of the push that was missed....

--- cpf/XRD_FitSubpattern.py
+++ cpf/XRD_FitSubpattern.py
@@ -729,7 +729,7 @@ def fit_sub_pattern(
         {
             "range": [
                 [data_as_class.tth.min(), data_as_class.tth.max()],
-                data_as_class.conversion([data_as_class.tth.min(), data_as_class.tth.max()]),
+                data_as_class.conversion(np.array(data_as_class.tth.min(), data_as_class.tth.max())),
             ]
         }
     )

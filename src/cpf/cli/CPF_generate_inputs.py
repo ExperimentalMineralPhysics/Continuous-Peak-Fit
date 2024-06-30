fee753a94a3c3d90a4209c38a86f858a05abc61d
Eu-Pin Tien <eu-pin.tien@diamond.ac.uk>
2024-06-30 18:04:53 +0100

    Applied Ruff formatting and linting standards to CPF_generate_inputs.py

--- src/cpf/cli/CPF_generate_inputs.py
+++ src/cpf/cli/CPF_generate_inputs.py
@@ -3,6 +3,7 @@
 import argparse
 from pathlib import Path
 
+
 def run():
     # Create a parser object to take additional arguments for this function
     parser = argparse.ArgumentParser(
@@ -14,11 +15,12 @@ def run():
 
     # Add (optional) argument to decide where to write the example input file to
     parser.add_argument(
-        "-p", "--path",
-        default = ".",  # Save in current working directory if not called
+        "-p",
+        "--path",
+        default=".",  # Save in current working directory if not called
         type=str,
         # Will be displayed when "help" is called
-        help="Specify the directory in which the example input file is generated"
+        help="Specify the directory in which the example input file is generated",
     )
 
     args = parser.parse_args()
@@ -42,9 +44,15 @@ def run():
                 file.write("# Calibration and masking.\n")
                 file.write("Calib_type   = 'Dioptas'\n")
                 file.write("Calib_detector = 'Pilatus1M'\n")
-                file.write("Calib_data     = datafile_directory + 'CeO2_Pil207_E30_2Nov2016_001.tif'\n")
-                file.write("Calib_param    = datafile_directory + 'CeO2_cal_Dioptas.poni'\n")
-                file.write("Calib_mask     = datafile_directory + 'DiffractionMask_Dioptas.mask'\n")
+                file.write(
+                    "Calib_data     = datafile_directory + 'CeO2_Pil207_E30_2Nov2016_001.tif'\n"
+                )
+                file.write(
+                    "Calib_param    = datafile_directory + 'CeO2_cal_Dioptas.poni'\n"
+                )
+                file.write(
+                    "Calib_mask     = datafile_directory + 'DiffractionMask_Dioptas.mask'\n"
+                )
                 file.write("Calib_pixels = 172\n\n")
 
                 file.write("# Fitting properties for peaks.\n")
@@ -71,7 +79,9 @@ def run():
                 file.write("             'symmetry': 2\n")
                 file.write("           }],\n")
                 file.write("         'range': [[10.8, 11.67]],\n")
-                file.write("         'PeakPositionSelection': [[1,-170.5,11.19],[1,-150.5,11.19],[1,-120.5,11.19],[1,-58.997,11.19],[1,59.289,11.19],[1,23.187,11.19],[1,23.212,11.19], [1,53.158,11.19], [1,73.246,11.19], [1,93.246,11.19], [1,123.246,11.19], [2,-170, 11.54], [2,-150, 11.54], [2,-110, 11.54], [2,-90, 11.54], [2,-45, 11.54], [2,-0, 11.54], [2,45, 11.54], [2,90, 11.54], [2,120, 11.54], [2,150, 11.54], [2,179, 11.54]]\n")
+                file.write(
+                    "         'PeakPositionSelection': [[1,-170.5,11.19],[1,-150.5,11.19],[1,-120.5,11.19],[1,-58.997,11.19],[1,59.289,11.19],[1,23.187,11.19],[1,23.212,11.19], [1,53.158,11.19], [1,73.246,11.19], [1,93.246,11.19], [1,123.246,11.19], [2,-170, 11.54], [2,-150, 11.54], [2,-110, 11.54], [2,-90, 11.54], [2,-45, 11.54], [2,-0, 11.54], [2,45, 11.54], [2,90, 11.54], [2,120, 11.54], [2,150, 11.54], [2,179, 11.54]]\n"
+                )
                 file.write("},\n")
                 file.write("       {\n")
                 file.write("         'background': [0,0],\n")
@@ -113,7 +123,9 @@ def run():
                 file.write("              'width': 0,\n")
                 file.write("              'symmetry': 2\n")
                 file.write("            }],\n")
-                file.write("#          'PeakPositionSelection': [[1,-120.5,23.226],[1,-58.997,23.186],[1,59.289,23.208],[1,23.187,120.893],[1,23.212,6.367], [1,23.158,90.025], [1,23.246,25.729]]\n")
+                file.write(
+                    "#          'PeakPositionSelection': [[1,-120.5,23.226],[1,-58.997,23.186],[1,59.289,23.208],[1,23.187,120.893],[1,23.212,6.367], [1,23.158,90.025], [1,23.246,25.729]]\n"
+                )
                 file.write("          'range': [[23.1, 23.5]]\n")
                 file.write("        },\n")
                 file.write("       {\n")
@@ -144,6 +156,7 @@ def run():
 
         except PermissionError:
             print("Write permission required")
+
 
 # Run script if called directly
 if __name__ == "__main__":

dbb2f25783962f5c79031742c195525af9e86b48
S-Hunt <7522838+S-Hunt@users.noreply.github.com>
2025-04-17 13:12:28 +0100

    bug fix
    Correctly label the output file

--- src/cpf/XRD_FitPattern.py
+++ src/cpf/XRD_FitPattern.py
@@ -752,7 +752,8 @@ def execute(
                 filename = make_outfile_name(
                     settings_for_fit.image_list[j],
                     directory=settings_for_fit.output_directory,
-                    extension="integrated.png",
+                    additional_text = "integrated",
+                    extension=".png",
                     overwrite=True,
                 )
  

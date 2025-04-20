01256e275dcd06a2e191232bf44a4866aa06c8ce
Eu-Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-04-20 19:38:49 +0100

    Updated type hints for 'report' parameter to take only the known CPFLogger levels

--- src/cpf/settings.py
+++ src/cpf/settings.py
@@ -19,9 +19,6 @@ from cpf.IO_functions import (
     image_list,
     json_numpy_serializer,
 )
-
-# , get_output_options, detector_factory, register_default_formats
-from cpf.logging import CPFLogger
 from cpf.series_functions import (
     coefficient_type_as_number,
     coefficient_type_as_string,
@@ -29,10 +26,13 @@ from cpf.series_functions import (
     get_number_coeff,
 )
 
+# , get_output_options, detector_factory, register_default_formats
+from cpf.util.logging import get_logger
+
 # import logging
 
 # logger = logging.getLogger(__name__)
-logger = CPFLogger("cpf.settings")
+logger = get_logger("cpf.settings")
 
 # from cpf.XRD_FitPattern import logger
 
@@ -58,13 +58,20 @@ class Settings:
         self.outputs -- list of output processes to run
 
     """
-    print(logger.handlers)
 
     def __init__(
         self,
         settings_file: Optional[Path] = None,
         out_type=None,
-        report: bool = False,
+        report: Literal[
+            "DEBUG",
+            "EFFUSIVE",
+            "MOREINFO",
+            "INFO",
+            "WARNING",
+            "ERROR",
+            "CRITICAL",
+        ] = "INFO",
         debug: bool = False,
         mode: Literal["fit"] = "fit",
     ):
@@ -209,8 +216,6 @@ class Settings:
             Verbose outputs to find errors.
 
         """
-
-        print("settings populate", logger.handlers)
 
         # Fail gracefully
         if settings_file is None:
@@ -473,7 +478,9 @@ class Settings:
 
     def validate_fit_orders(
         self,
-        report: bool = False,
+        report: Literal[
+            "DEBUG", "EFFUSIVE", "MOREINFO", "INFO", "WARNING", "ERROR"
+        ] = "INFO",
         peak=None,
         orders=None,
     ):
@@ -745,7 +752,9 @@ class Settings:
         peak_set,
         peak,
         component,
-        report: bool = False,
+        report: Literal[
+            "DEBUG", "EFFUSIVE", "MOREINFO", "INFO", "WARNING", "ERROR"
+        ] = "INFO",
     ):
         """
         Checks that a set component type is valid -- i.e. it exists in PeakFunctions.
@@ -787,7 +796,9 @@ class Settings:
         peak_set,
         peak,
         component,
-        report: bool = False,
+        report: Literal[
+            "DEBUG", "EFFUSIVE", "MOREINFO", "INFO", "WARNING", "ERROR"
+        ] = "INFO",
     ):
         """
         Checks that a fixed component set is the same size of the orders that govern it.
@@ -865,7 +876,9 @@ class Settings:
     def validate_position_selection(
         self,
         peak_set: int | list[int] = 0,
-        report: bool = False,
+        report: Literal[
+            "DEBUG", "EFFUSIVE", "MOREINFO", "INFO", "WARNING", "ERROR"
+        ] = "INFO",
     ):
         """
         Checks that the multiple peak position selections have the right number of parts.
@@ -930,7 +943,9 @@ class Settings:
 
     def validate_fit_bounds(
         self,
-        report: bool = False,
+        report: Literal[
+            "DEBUG", "EFFUSIVE", "MOREINFO", "INFO", "WARNING", "ERROR"
+        ] = "INFO",
     ):
         """
         check the peak fitting bounds in the input file are valid.
@@ -979,13 +994,14 @@ class Settings:
             else:
                 logger.error(" ".join(map(str, [(err_str)])))
         else:
-            print("here")
             logger.info(" ".join(map(str, [("fit_bounds appears to be correct")])))
 
     def set_output_types(
         self,
         out_type_list: list[str] = [],
-        report: bool = False,
+        report: Literal[
+            "DEBUG", "EFFUSIVE", "MOREINFO", "INFO", "WARNING", "ERROR"
+        ] = "INFO",
     ):
         """
         set output types in settings class given a list of output types

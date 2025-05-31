7a84f8ff1b9d4da927f565e13acfe566f29f21fe
S-Hunt <7522838+S-Hunt@users.noreply.github.com>
2025-05-31 16:03:09 +0100

    Documentation update

--- src/cpf/peak_functions.py
+++ src/cpf/peak_functions.py
@@ -16,6 +16,10 @@ __all__ = [
     "pseudo_voigt_peak",
 ]
 
+
+__doc__ = "Functions for Pseudo-Voigt peak shape. "
+
+
 import numpy as np
 
 from cpf.util.logging import get_logger
@@ -26,13 +30,26 @@ logger = get_logger("cpf.peak_functions")
 
 def peak_components(full=False, include_profile=True):
     """
-    Lists the parameters needed for each peak.
-    If full is True returns all the parameters needed for the fit (including background and symetry)
-    Otherwise returns just the peak parameters
+    Lists the parameters needed for the peak shape function.
+    If 'full' is True returns all the parameters needed for the fit (including background and symmetry)
+    Otherwise returns just the peak parameters.
+    
 
-    :param full:
-    :return comp_list:
-    :return comp_names:
+    Parameters
+    ----------
+    full : bool, optional
+        Return just the parameters for the peak shape (if False) or with additional 
+        parameters (if True). The default is False.
+    include_profile : bool, optional
+        Include the peak shape 'profile' in the output. The default is True.
+
+    Returns
+    -------
+    comp_list : list
+        List of peak component 1 letter abbreviations.
+    comp_names : list
+        List of peak component full names.
+
     """
     comp_list = ["h", "d", "w"]
     comp_names = ["height", "d-space", "width"]
@@ -50,11 +67,26 @@ def peak_components(full=False, include_profile=True):
 
 def expand_component_string(comp):
     """
-    Exapand he compnent name from its short representation to its long name.
+    Exapand he compnent name from its short representation to its full name.
     e.g. "d" --> "d-spance"
+    
+    Conversion is performed using lookup of outputs from peak_functions.peak_components()
 
-    :param component name:
-    :return: expanded component name
+    Parameters
+    ----------
+    comp : str
+        Single letter peak shape component.
+
+    Raises
+    ------
+    ValueError
+        Unrecognised peak property type.
+
+    Returns
+    -------
+    out : str
+        Full name for peak shape component in input.
+
     """
     comp_list, comp_names = peak_components(full=True)
     if comp in comp_names:
@@ -63,31 +95,30 @@ def expand_component_string(comp):
         out = comp_names[comp_list.index(comp)]
     else:
         raise ValueError("Unrecognised peak property type")
-
-    # if comp == "d" or comp == "d-space":
-    #     out = "d-space"
-    # elif comp == "h" or comp == "height":
-    #     out = "height"
-    # elif comp == "w" or comp == "width":
-    #     out = "width"
-    # elif comp == "p" or comp == "profile":
-    #     out = "profile"
-    # elif comp == "bg" or "f" or comp == "background":
-    #     out = "background"
-    # else:
-    #     raise ValueError("Unrecognised peak property type")
     return out
 
 
 # Gaussian shape
 def gaussian_peak(two_theta, two_theta_0, w_all, h_all):
     """
-    Gaussian shape
-    :param two_theta:
-    :param two_theta_0:
-    :param w_all:
-    :param h_all:
-    :return:
+    Calculates intensiities for Gaussian peak shape
+
+    Parameters
+    ----------
+    two_theta : np.array
+        Array of all two theat values to calculate intensity for.
+    two_theta_0 : np.array
+        Centroid of the peak at each two theta.
+    w_all : np.array
+        Width of the peak at each two theta value.
+    h_all : np.array
+        Height of the peak at each two theta value.
+
+    Returns
+    -------
+    gauss_peak : np.array
+        Intensity of the Gaussian peak at each two theta value.
+
     """
     w_all = w_all / np.sqrt(np.log(4))
     gauss_peak = h_all * np.exp((-((two_theta - two_theta_0) ** 2)) / (2 * w_all**2))
@@ -96,28 +127,55 @@ def gaussian_peak(two_theta, two_theta_0, w_all, h_all):
 
 def lorentzian_peak(two_theta, two_theta_0, w_all, h_all):
     """
-    Lorentz shape
-    :param two_theta:
-    :param two_theta_0:
-    :param w_all:
-    :param h_all:
-    :return:
+    Calculates intensiities for Lorentz peak shape
+
+    Parameters
+    ----------
+    two_theta : np.array
+        Array of all two theat values to calculate intensity for.
+    two_theta_0 : np.array
+        Centroid of the peak at each two theta.
+    w_all : np.array
+        Width of the peak at each two theta value.
+    h_all : np.array
+        Height of the peak at each two theta value.
+
+    Returns
+    -------
+    lorentz_peak : np.array
+        Intensity of the Lorentzian peak at each two theta value.
+
     """
-    l_peak = h_all * w_all**2 / ((two_theta - two_theta_0) ** 2 + w_all**2)
-    return l_peak
+    lorentz_peak = h_all * w_all**2 / ((two_theta - two_theta_0) ** 2 + w_all**2)
+    return lorentz_peak
 
 
 def pseudo_voigt_peak(two_theta, two_theta_0, w_all, h_all, l_g_ratio):
     """
-    Pseudo-Voigt
-    :param two_theta:
-    :param two_theta_0:
-    :param w_all:
-    :param h_all:
-    :param l_g_ratio:
-    :return:
+    Calculates intensiities for Lorentz peak shape
+
+    Parameters
+    ----------
+    two_theta : np.array
+        Array of all two theat values to calculate intensity for.
+    two_theta_0 : np.array
+        Centroid of the peak at each two theta.
+    w_all : np.array
+        Width of the peak at each two theta value.
+    h_all : np.array
+        Height of the peak at each two theta value.
+    l_g_ratio : np.array
+        Proportions of Gauss and Lorentz peak at each two theta value.
+        if l_g_ratio = 1 returns Gaussian peak 
+        if l_g_ratio = 0 returns Lorentzian peak 
+        
+    Returns
+    -------
+    PesudoVoigt_peak : np.array
+        Intensity of the Pesudo-Voigt peak at each two theta value.
+
     """
-    p_v_peak = l_g_ratio * gaussian_peak(two_theta, two_theta_0, w_all, h_all) + (
+    PesudoVoigt_peak = l_g_ratio * gaussian_peak(two_theta, two_theta_0, w_all, h_all) + (
         1 - l_g_ratio
     ) * lorentzian_peak(two_theta, two_theta_0, w_all, h_all)
-    return p_v_peak
+    return PesudoVoigt_peak

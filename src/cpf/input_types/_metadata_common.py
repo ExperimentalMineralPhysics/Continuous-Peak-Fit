e94a8cda54edb03c1ba85d3365efb81c0ec18db6
S-Hunt <7522838+S-Hunt@users.noreply.github.com>
2026-05-31 17:15:25 +0100

    Update to metadata functions and documentation
    Organise the metadata better.
    Also write a set of documentation for how the metadata works.

--- src/cpf/input_types/_metadata_common.py
+++ src/cpf/input_types/_metadata_common.py
@@ -17,52 +17,139 @@ from cpf.util.logging import get_logger
 logger = get_logger("cpf.input_types._metadata")
 
 
+"""
+Metadata in continous peak fit.
+===============================
+
+Cpf has the options to read metadata from the diffraction files and record it with the fits in the json files and is added to the output files.
+Metadata that is required by an output type listed in the Settings() is by default added to the saved metadata.
+
+The 'settings.metadata' is a list of metadata parameter names that will be added to the {fits}.json files as key,value pairs. 
+This is formed from:
+    (a) the 'metadata' list in the inputfile.
+    (b) names required by the outputs, as held in settings.metadata_labels dictionary (see below)
+    (c) hdf5 keys in h5_iterate (see below and hdf5 documentation).
+    (d) keys returned by metadata_func
+
+For non-hdf5 file types all the metadata is imported from the image file along with the diffraction data by data_class.import_image() 
+and is stored in data_class.metadata.
+For hdf file image types the number of possible metdata data values is vast, so only the named values are given.
+
+Only the named metadata is added to the saved files. 
+
+
+Possible metadata names
+-----------------------
+
+Possible metadata parameters are anything listed in the data file and accessible by the data_class.  
+
+For hdf5 type files, any valid key is allowed but needs the same number of '*' wild cards to get metadata corresponding to the right images (see h5 documentation).
+
+There is also a set of metadata values designed for use with compound diffraciton images (e.g. ESRF lvp detector, or multiple exposures making a single diffraction collection)
+These also work for single frame images and are derived using default "time" and 'exposure' metadata_labels 
+The options for this are:
+    - "frames start"    -- start time of data collection
+    - "frames mid"      -- median time of data collection, accounting for the exposre time
+    - "frames end"      -- end time of data collection, accounting for the exposre time
+    - "frames exposure" -- total exposure time of data collection, accounting for the frame time
+If no exposure time is specified then only "frames start" is returned.
+
+A generic backstop of times is also avaliable in for form of:
+    - 'FILE_CREATION'  -- file creation time read from operatiing system
+    - 'FILE_MODIFIED'  -- file modification time read from operatiing system
+
+
+metadata_labels and RequiredParams
+----------------------------------
+
+The metadata names or locations are specific to the data type. For example, the time of the data collection trigger or acquition time are called different 
+names by different data classes and by different synchroton facilites. 
+Therefore parameters called by Write{name of output}.Requirements()["RequiredParams"] are generic pointers, that are speficifed in the data_class.
+Each data_class has attribute 'metadata_labels', which is a dictionary that points the RequiredParams to the specific metadata location.
+for example:
+    data_class.metadata_labels = {"time":        "mean_start_time",
+                                  'exposure':    'mean_live_time',
+                                  "temperature": "*LVP_tc1_calcs.I"}
+These values are set by:
+    - initially from data_class._default_metadata_labels*
+    - overriding the defaults with values from 'metadata_labels' in input
+
+Dictionary keys from data_class.metadata_labels are added to settings.metadata and saved to json fits files. 
+
+
+Adding external matadata values -- metadata_read_func
+-----------------------------------------------------
+
+metadata_read_func is a method defined by the input that allows interation with external (to the data files) sources of metadata.
+It is added to Settings as:
+    Settings.metadata_read_func()
+from function called `metadata_read_func' in the input.
+
+It returns a dictionary of values to be added to the metadata. The method must:
+ - accept 3 optional arguments (settings, image_obj, filename) and kwargs
+ - return dictionary of metadata key, value pairs
+ - return a dictionary of the same key, value pairs even if the inputs are absent 
+        or fail. The empty dict is used to determine the metadata to add to the outputs. 
+
+An example is: 
+
+code block:
+        def metadata_read_func(settings=None, image_obj=None, filename=None, **kwargs):
+            # example of a metadata_read_func     
+            # taken from Example 1
+            try:
+                hd = image_obj.header
+                out = {}
+                out["Exposure_time"] = hd["Exposure_time"]
+                out["Exposure_period"] = hd["Exposure_period"]
+                out['namename'] = image_obj.filename
+                out['daft'] = "Needless string"
+            except:
+                out = {}
+                out["Exposure_time"] = None
+                out["Exposure_period"] = None
+                out['namename'] = None
+                out['daft'] = "Needless string"
+                
+            return out
+        
+If the outputs of metadata_read_func are required by the outputs, then 'data_calss.metadata_labels' values can be modified to use them.
+
+
+TO DO:
+    1. rename metadata_labels as metadata_names ?? 
+    2. write a data_class.metadata_possibilities() method that lists all possible metadata names. 
+    3. change definitions of metadata_read_func. strictly it does not need all three of the forced parameters and infact never uses them all 
+"""
 
 class _metadata_common:
-    """
-    Class definiing metadata function(s).
-
-    These are imported into the detector functions as methods.
-
-    In the settings class and detector functions (referred to as data_class in the code) the metadata should work as follows:
-
-    settings.metadata_labels:
-        This is a dictionary that associates the metadata types needed by the output files to the metadata names in 
-        settings.metadata.
-        for example 
-        settings.metadata_labels = {"temperature": "tc1"} will use "tc1" from the metadata when temperature is called in the outputs. 
-
-    settings_class.metadata_labels are set during the settings class initiate from:
-        (a) dataclass._default_metadata_labels and overridden by 
-        (b) 'metadata_labels' in the input file
-    The default values set are 'time', temperature' and 'exposure'.
-    
-
-    settings.metadata:
-        is the list of metadata parameters that are to be read from the data files. 
-        The list is composed from (a) the 'metadata' values in the input file and the required values from the 
-
-
-
-    """
-    
+            
     def get_metadata(self, settings_class= None, metadata_values="default", report=None):
         """
-        Gets metadata as from data_class.metadata.
+        Gets speficied metadata as from settings_class.metadata and returns as a dictionary.
+        
         The default is to get the timestamps of the images but other data 
         stored in the file can be accessed as well with the correct label.
         
         For h5 files any hdf5 key is allowed but needs '*' wild cards to move through the dataset
         with the images (see h5 documentation).
-        For edf and other file formats anything that is a property accessible through fabio image.header is permitted.
+        For edf and other file formats any property accessible through fabio image.header is permitted.
         
         Otherfile types are parsed through fabio and behave accordingly.
     
         The implemented options for the metadata parameters are:
-        - "time start" -- gives start time of data collection
-        - "time mid" -- gives middle time of data collection, accounting for the exposre time
-        - "time end" -- gives end time of data collection, accounting for the exposre time
+            - "frames start"    -- start time of data collection
+                                        min(timestamps)
+            - "frames mid"      -- median time of data collection, accounting for the exposre time
+                                        np.median(timestamps+exposures)
+            - "frames mean"     -- mean time of data collection, accounting for the exposre time  
+                                        (timestamps+exposures) / len(timestamps)
+            - "frames end"      -- end time of data collection, accounting for the exposre time
+                                        max(timestamps+exposures)
+            - "frames exposure" -- total exposure time of data collection, accounting for the frame time
+                                        max(timestamps+exposures) - min(timestamps)
         If no exposure time is determined then only "time_start" is returned.
+       
     
         Parameters
         ----------
@@ -98,7 +185,8 @@ class _metadata_common:
         
         no_exposure_message = None#"no exposure"
         # options for times
-        time_opts = ["time_mid" ,"time_start" ,"time_end", "time_exposure"]
+        time_opts = ["frames start", "frames mid", "frames mean", "frames end", "frames exposure"]
+        # time_opts = ["time_mid" ,"time_start" ,"time_end", "time_exposure"]
         
         # make output dictionary
         metadata_out = {}
@@ -112,7 +200,7 @@ class _metadata_common:
         time_location = self.metadata_labels.get('time', self._default_metadata_labels.get('time',None))
         exposure_location = self.metadata_labels.get('exposure', self._default_metadata_labels.get('exposure', None))
         
-        #parse metadata_values list 
+        #parse metadata_values list
         discard = []
         if time_location not in metadata_values:
             discard.append(time_location)
@@ -121,7 +209,7 @@ class _metadata_common:
             replaced = list(set(metadata_values) & set(time_opts))
             metadata_values = list(set(metadata_values) - set(time_opts))
             metadata_values.append(time_location)
-            if (("time_end" in replaced or "time_mid" in replaced or "time" in replaced)
+            if (("frames end" in replaced or "frames mid" in replaced or "time" in replaced)
                 and exposure_location is not None):
                 # stop
                 if exposure_location not in metadata_values:
@@ -211,40 +299,26 @@ class _metadata_common:
                         except:
                             metadata_out[k] = self.metadata[k]
                 else:
-                    err_str = f"Metadata type '{j}' not recognised. Permitted values for this dataset are: {headers}."
+                    err_str = f"Metadata type '{j}' not recognised. Permitted values for this dataset are: {headers.append(time_opts)}."
                     raise ValueError(err_str)
             
         if replaced != None:
-            if "time" in replaced:
-                if exposure_location is not None:
-                    metadata_out["time"] = time_combine(metadata_out[time_location], metadata_out[exposure_location], second_time_scale=1/2)
-                else: 
-                    metadata_out["time"] = no_exposure_message
-            if "time_mid" in replaced:
-                if exposure_location is not None:
-                    metadata_out["time_mid"] = time_combine(metadata_out[time_location], metadata_out[exposure_location], second_time_scale=1/2)
-                else: 
-                    metadata_out["time_mid"] = no_exposure_message
-            if "time_start" in replaced:
-                metadata_out["time_start"] = time_combine(metadata_out[time_location])
-                    # metadata_out["time_start"] = time_combine(self.metadata[time_location])
-            if "time_end" in replaced:
-                if exposure_location is not None:
-                    if isinstance(metadata_out[time_location], list):
-                        last = metadata_out[time_location][-1]
-                        last_exp = metadata_out[exposure_location][-1]
-                    else:
-                        last = metadata_out[time_location]
-                        last_exp = metadata_out[exposure_location]
-                    metadata_out["time_end"] = time_combine(last, last_exp, second_time_scale=1)
-                else: 
-                    metadata_out["time_end"] = no_exposure_message
-            if "time_exposure" in replaced:
-                metadata_out["time_exposure"] = metadata_out.get(exposure_location, no_exposure_message)
-                # if exposure_location is not None:
-                #     metadata_out["time_exposure"] = self.metadata[exposure_location]
-                # else: 
-                #     metadata_out["time_exposure"] = no_exposure_message
+            # if "time" in replaced:
+            #     if exposure_location is not None:
+            #         metadata_out["time"] = times_combine(metadata_out[time_location], metadata_out[exposure_location])
+            #     else:
+            #         metadata_out["time"] = times_combine(metadata_out[time_location])
+            if "frames start" in replaced:
+                metadata_out["frames start"] = metadata_combine(metadata_out, time_location, exposure_location, get="start", as_str=False)
+            if "frames mid" in replaced:
+                metadata_out["frames mid"] = metadata_combine(metadata_out, time_location, exposure_location, get="mid", as_str=False)
+            if "frames mean" in replaced:
+                metadata_out["frames mean"] = metadata_combine(metadata_out, time_location, exposure_location, get="mean", as_str=False)
+            if "frames end" in replaced:
+                metadata_out["frames end"] = metadata_combine(metadata_out, time_location, exposure_location, get="end", as_str=False)
+            if "frames exposure" in replaced:
+                metadata_out["frames exposure"] = metadata_combine(metadata_out, time_location, exposure_location, get="exposure", as_str=False)
+                # metadata_out["frames exposure"] = metadata_out.get(exposure_location, no_exposure_message)
             for k in discard:
                 metadata_out.pop(k, None)
             
@@ -319,57 +393,88 @@ class _metadata_common:
         
         return metadata_dict
         
-        
 
-def time_combine(first_time, second_time=0, second_time_scale=1):
+def metadata_combine(metadata, time_location, exposure_location=None, get="mean", as_str=False):
     """
-    Combine two times and return in the same format (string or time) as first_time
-
+    Combine multiple time stamps and return value in the same format (string or time) as first_time
+    
+    The values for get are:
+        - "start"    -- start time of data collection
+                                    min(timestamps)
+        - "mid"      -- median time of data collection, accounting for the exposre time
+                                    np.median(timestamps+exposures)
+        - "mean"     -- mean time of data collection, accounting for the exposre time  
+                                    (timestamps+exposures) / len(timestamps)
+        - "end"      -- end time of data collection, accounting for the exposre time
+                                    max(timestamps+exposures)
+        - "exposure" -- total exposure time of data collection, accounting for the frame time
+                                    max(timestamps+exposures) - min(timestamps) 
+                                    
     Parameters
     ----------
-    how : TYPE
+    metadata : TYPE
         DESCRIPTION.
-    first_time : TYPE
-        DESCRIPTION.
-    second_time : TYPE
-        DESCRIPTION.
-    second_time_scale : TYPE, optional
-        DESCRIPTION. The default is None.
+    time_location : str
+        key string for time location in metadata dictionary.
+    exposure_location : str, optional
+        key string for exposure location in metadata dictionary. The default is None.
+    get : str, optional
+        which value to get. The default is "mean".
+    as_str : bool, optional
+        Force the timestamps to be strings; if true return in same format as timestamps.
+        The default is False.
+        
+    Raises
+    ------
+    ValueError
+        Unrecognised combination of values.
 
     Returns
     -------
-    None.
+    out_time : list, float, str
+        Require time stamp(s) in the same format as the input.
 
     """
-    #format of time string
-    as_str = False
-    if isinstance(first_time, list):
+    
+    # parse inputs
+    timestamps = metadata[time_location], 
+    if exposure_location:
+        exposures = metadata[exposure_location]
+    else:
+        exposures = None
+    
+    if isinstance(timestamps, str):
+        timestamps = [timestamps]
+    if exposures == None:
+        exposures = 0
+    
+    #get input format
+    if isinstance(timestamps[0], str):
         as_str = True
-        for i in range(len(first_time)):
-            if isinstance(first_time[i], str):
-                first_time[i] = parse(first_time[i]).timestamp()
-    elif isinstance(first_time, str):
-        as_str = True
-        first_time = parse(first_time).timestamp()
-    first_time = np.nanmean(first_time)
-    if isinstance(second_time, list):
-        for i in range(len(second_time)):
-            if isinstance(second_time[i], str):
-                second_time[i] = parse(second_time[i]).timestamp()
-    elif isinstance(second_time, str):
-        as_str = True
-        second_time = parse(second_time).timestamp()
-    second_time = np.nanmean(second_time)
         
-    out_time = first_time + second_time * second_time_scale
+    # force all values to be numbers
+    for i in range(len(timestamps)):
+        timestamps = [parse(v).timestamp() if isinstance(v, str) else v for v in timestamps]
         
-    if as_str:
+    match get:
+        case "start":
+             out_time = np.min(np.array(timestamps))
+        case "mid":
+             out_time = np.median(np.array(timestamps)+np.array(exposures))
+        case "mean":
+             out_time = np.sum(np.array(timestamps)+np.array(exposures)) / len(timestamps)
+        case "end":
+             out_time = np.max(np.array(timestamps)+np.array(exposures))
+        case "exposure":
+             out_time = np.max(np.array(timestamps)+np.array(exposures)) - np.min(np.array(timestamps))
+        case _:
+            raise ValueError("Unrecognised combination type")
+        
+    if as_str and get != "exposure":
         out_time = f"{datetime.fromtimestamp(out_time):%Y-%d-%b %H:%M:%S.%f}"
-        
-    return out_time
     
+    if exposure_location is None:
+        return None
+    else:
+        return out_time
     
-    
-    
-    
-

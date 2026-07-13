numeric precision bug fix
the data minimumPrecision is set to 32 to prevent numpy.str returning inf when sum is greater than maximum allowed by 16 bit precision. see:
https://github.com/numpy/numpy/issues/22448
not setting this can cause inf in XRD_FitPattern when comparing the height of the peaks to the standard deviation of the data --currently line 525.

src/cpf/input_types/_AngleDispersive_common.py
@@ -287,7 +287,7 @@ class _AngleDispersive_common:
             A numpy data type to be applied to the data arrays.
 
         """
-        precision = re.findall("\d+", rawData.dtype.name)[0]
+        precision = int(re.findall("\d+", rawData.dtype.name)[0])
         # force minimum precision
         if minimumPrecision != False:
             if precision < minimumPrecision:
@@ -296,7 +296,7 @@ class _AngleDispersive_common:
             pass
 
         try:
-            DataType = np.dtype(numType + precision)
+            DataType = np.dtype(numType + str(precision))
         except:
             err_str = f"The datatype, {DataType}, is not a recognised data type"
             logger.critical(err_str)
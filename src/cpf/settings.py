09aeeef9815c9dbf8aa60514feb12928a86c09d2
S-Hunt <7522838+S-Hunt@users.noreply.github.com>
2026-07-16 14:10:30 +0100

    bugfix
    fix for peak hkl if one is missing.

--- src/cpf/settings.py
+++ src/cpf/settings.py
@@ -945,7 +945,7 @@ class Settings:
                             and not "phase" in self.fit_orders[i]["peak"][j]
                         ):
                             self.fit_orders[i]["peak"][j]["phase"] = "Region"
-                            self.fit_orders[i]["peak"][j]["hkl"] = i + 1
+                            self.fit_orders[i]["peak"][j]["hkl"] = f'{i:03}'
 
                 if "PeakPositionSelection" in self.fit_orders[i]:
                     mssng = self.validate_position_selection(peak_set=i, report=report)

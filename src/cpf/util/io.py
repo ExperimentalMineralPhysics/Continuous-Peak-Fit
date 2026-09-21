Optimised logic for 'peak_string' function so that numerical inputs all get standardise to Python ints, and fixed outdated type hinting

src/cpf/util/io.py
@@ -766,7 +766,7 @@ def peak_string(
         the Miller indices in the form of a string or an integer array, as well as
         the 'phase' key, which contains information about which material this peak
         belongs to.
-    peak: int | list[int] | str | Literal['all']
+    peak: int | list[int] | Literal['all']
         Indices of the peak descriptors to use to generate the peak string with.
         Takes an integer, a list of integers, or 'all'. The default is 'all'.
     fname: bool
@@ -785,16 +785,12 @@ def peak_string(
     # Construct list of indices to parse
     if peak == "all":
         peaks = list(range(len(orders["peak"])))
-    # If a list of ints is provided
-    elif isinstance(peak, list) and all(isinstance(x, int) for x in peak):
-        peaks = peak
-    # If an int was provided
-    elif (
-        isinstance(peak, str)
-        or isinstance(peak, int)
-        or np.issubdtype(peak, np.integer)
-    ):
-        peaks = [peak]
+    # If a list of ints or NumPy ints was provided
+    elif isinstance(peak, list) and all(isinstance(x, (int, np.integer)) for x in peak):
+        peaks = [int(p) for p in peak]  # Standardise all as Python int
+    # If an int or NumPy int was provided
+    elif isinstance(peak, (int, np.integer)):
+        peaks = [int(peak)]  # Standardise as Python int
     # Raise a TypeError otherwise
     else:
         raise TypeError(